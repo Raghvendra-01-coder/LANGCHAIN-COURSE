38759a8a6035303a47bf26e0352b714adeb6a661
Raghvendra-01-coder <kumariiitbang@gmail.com>
2025-09-19 07:56:26 +0530

    Check the Api Key

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()  # take environment variables from .env.
 def main():
     print("Hello from langchain-course!")
-
+    print('OPEN_API_KEY : ', os.getenv('OPEN_API_KEY'))
 
 if __name__ == "__main__":
     main()
